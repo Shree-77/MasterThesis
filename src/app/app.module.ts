8f5cea689a5a5e930a95478f2bcef701e1f9b770
shree-77 <shreedhoni7@gmail.com>
2023-10-15 08:59:55 +0530

    Files moved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ChartComponent } from './chart/chart.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FileUploadComponent } from './file.upload/file.upload.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ErrorComponent } from './error/error.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+AppComponent,ChartComponent,FileUploadComponent,LoginComponent, ErrorComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,HttpClientModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
